9a8a0a48ba6eda8428551a0485a0fbb994c81814
sriram <ggcsriram@gmail.com>
2019-12-16 16:43:48 +0530

    moor_setup

---
+++ lib/moor/students_database.dart
@@ -0,0 +1,26 @@
+import 'package:moor/moor.dart';
+import 'package:moor_flutter/moor_flutter.dart';
+
+part 'students_database.g.dart';
+
+class Students extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get name => text().withLength(min: 1, max: 50)();
+
+  @override
+  Set<Column> get primaryKey => {id};
+}
+
+@UseMoor(tables: [Students])
+class StudentsDatabase extends _$StudentsDatabase {
+  StudentsDatabase()
+      : super(FlutterQueryExecutor.inDatabaseFolder(
+            path: 'db.sqlite', logStatements: true));
+
+  @override
+  int get schemaVersion => 1;
+
+  Stream<List<Student>> getStudents() => select(students).watch();
+
+  Future addStudent(Student student)=>into(students).insert(student);
+}
